01c2638d23d95e6f9f33d1ca14c77d68c3056a29
wangyanbo29 <wangyanbo29@jd.com>
2023-07-30 17:43:10 +0800

    canal

---
+++ canal/src/test/java/com/yienx/canal/CanalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yienx.canal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
